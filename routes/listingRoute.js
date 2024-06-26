814928cba87c5176da7e2df4369ffec6a7b4ea7d
Abdallahaweda <abdallahslahaweda@gmail.com>
2024-06-25 19:57:27 -0700

    create search api

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -3,7 +3,8 @@ import {
   createListing,
   deleteListing,
   updateListing,
-  showListing
+  showListing,
+  getListings,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -12,6 +13,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-router.get("/show/:id",  showListing);
-
+router.get("/show/:id", showListing);
+router.get("/get", getListings);
 export default router;
