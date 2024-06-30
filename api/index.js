3d27a13239b3bcccec295278d2aa9d53261785c8
Abdallahaweda <abdallahslahaweda@gmail.com>
2024-06-29 23:53:57 -0700

    v4

--- api/index.js
+++ api/index.js
@@ -25,9 +25,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -54,3 +54,5 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+export const handler = ServerlessHttp(app);
