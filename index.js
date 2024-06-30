3e8728408b9c1471e4c25f22a0b8a343893ca0e3
Abdallahaweda <abdallahslahaweda@gmail.com>
2024-06-29 23:03:32 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dontenv from "dotenv";
-import userRouter from "../routes/UserRoute.js";
-import authRouter from "../routes/AuthRoute.js";
-import listinRouter from "../routes/listingRoute.js";
+import userRouter from "./routes/UserRoute.js";
+import authRouter from "./routes/AuthRoute.js";
+import listinRouter from "./routes/listingRoute.js";
 import cookieParser from "cookie-parser";
 import ServerlessHttp from "serverless-http";
 
